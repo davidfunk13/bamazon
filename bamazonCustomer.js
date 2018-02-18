6ac4380c1f139c9bc38c6347e9a599688bd12711
Dave <davefunk135@gmail.com>
2018-02-18 14:42:15 -0800

    switch case for ten items added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,7 +10,6 @@ inquirer.prompt({
     console.log(`Welcome ${answers.name}!`);
     console.log(`---------------------------------`);
     getAll();
-    askForProductID();
 });
 
 var bamazonConnection = mysql.createConnection({
@@ -37,22 +36,57 @@ function getAll() {
             console.log(`Item ID: ${response[i].item_id}\n\nProduct Name: ${response[i].product_name}\nPrice: ${response[i].price}\n`);
             console.log('-----------------------------')
         }
-        bamazonConnection.end(function(error) {
+        bamazonConnection.end(function (error) {
             if (error) {
                 console.log(error);
             }
             console.log(`query made and connection ended successfully`)
-        })
-    })
-}
+            askForProductID();
+        });
+    });
+};
 
 function askForProductID() {
     inquirer.prompt({
         type: 'list',
-        name: 'what_product',
-        message: 'What is the ID of the product you would like to purchase?',
-        choices: [1,2,3,4,5,6,7,8,9,10]
+        name: 'productid',
+        message: 'What is the Item ID of the product you wish to purchase?',
+        choices: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
     }).then(answers => {
-        console.log(answers);
+        switch (answers.productid) {
+            case '1':
+                console.log('case 1')
+                break;
+            case '2':
+                console.log('case 2')
+                break;
+            case '3':
+                console.log('case 3');
+                break;
+            case '4':
+                console.log('case 4');
+                break;
+            case '5':
+                console.log('case 5');
+                break;
+            case '6':
+                console.log('case 6');
+                break;
+            case '7':
+                console.log('case 7');
+                break;
+            case '8':
+                console.log('case 8');
+                break;
+            case '9':
+                console.log('case 9');
+                break;
+                case '10':
+                console.log('case 10');
+                break;
+            default:
+                console.log('asdasda')
+        }
     })
 }
+
