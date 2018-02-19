e7ed411af42985db78c10d1412d8d0cbe5ee1cba
Dave <davefunk135@gmail.com>
2018-02-18 18:14:14 -0800

    cleaned up a lot, wrapped things into functions, now using database information to check stock.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,14 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 
+var bamazonConnection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "dave",
+    password: "dbpass",
+    database: "bamazon"
+});
+
 inquirer.prompt({
     type: 'input',
     name: 'name',
@@ -12,21 +20,29 @@ inquirer.prompt({
     getAll();
 });
 
-var bamazonConnection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "dave",
-    password: "dbpass",
-    database: "bamazon"
-});
+function connectToBamazon() {
+    bamazonConnection.connect(function (error) {
+        if (error) {
+            console.error(error);
+        }
+        console.log('connected');
+    });
+}
 
-bamazonConnection.connect(function (error) {
-    if (error) {
-        console.error(error);
-    }
-});
+function disconnectFromBamazon() {
+    bamazonConnection.end(function (error) {
+        if (error) {
+            console.log(error);
+        }
+        console.log(`query made and connection ended successfully`)
+    });
+}
+
+
+
 
 function getAll() {
+    connectToBamazon();
     bamazonConnection.query("SELECT * from products", function (error, response) {
         if (error) {
             console.error(error);
@@ -36,14 +52,9 @@ function getAll() {
             console.log(`Item ID: ${response[i].item_id}\n\nProduct Name: ${response[i].product_name}\nPrice: ${response[i].price}\n`);
             console.log('-----------------------------')
         }
-        bamazonConnection.end(function (error) {
-            if (error) {
-                console.log(error);
-            }
-            console.log(`query made and connection ended successfully`)
-            askForProductID();
-        });
     });
+    // disconnectFromBamazon()
+    askForProductID();
 };
 
 function askForProductID() {
@@ -55,62 +66,82 @@ function askForProductID() {
     }).then(answers => {
         switch (answers.productid) {
             case '1':
+                var productID = answers.productid;
                 console.log('case 1')
-                askQuantity();
+                askQuantity(productID);
                 break;
             case '2':
+                var productID = answers.productid;
                 console.log('case 2')
-                askQuantity();
+                askQuantity(productID);
                 break;
             case '3':
+                var productID = answers.productid;
                 console.log('case 3');
-                askQuantity();
+                askQuantity(productID);
                 break;
             case '4':
+                var productID = answers.productid;
                 console.log('case 4');
-                askQuantity();
+                askQuantity(productID);
                 break;
             case '5':
+                var productID = answers.productid;
                 console.log('case 5');
                 askQuantity();
                 break;
             case '6':
+                var productID = answers.productid;
                 console.log('case 6');
-                askQuantity();
+                askQuantity(productID);
                 break;
             case '7':
+                var productID = answers.productid;
                 console.log('case 7');
-                askQuantity();
+                askQuantity(productID);
                 break;
             case '8':
+                var productID = answers.productid;
                 console.log('case 8');
-                askQuantity();
+                askQuantity(productID);
                 break;
             case '9':
+                var productID = answers.productid;
                 console.log('case 9');
-                askQuantity();
+                askQuantity(productID);
                 break;
-                case '10':
+            case '10':
+                var productID = answers.productid;
                 console.log('case 10');
-                askQuantity();
+                askQuantity(productID);
                 break;
             default:
                 console.log('asdasda')
         }
     })
 }
-function askQuantity () {
+function askQuantity(productID) {
+    console.log(productID)
     inquirer.prompt({
         type: 'input',
         name: 'quantity',
         message: 'How many of this iten would you like to order?'
-
     }).then(answers => {
         if (isNaN(answers.quantity)) {
             console.log('That isnt a number.')
             askQuantity();
         }
-        console.log(answers.quantity);
+        checkDatabase(productID);
     });
+}
+function checkDatabase(productID) {
+    console.log(`checkdatabase function: ${productID}`)
+    bamazonConnection.query("SELECT * from products", function (error, response) {
+        if (error) {
+            console.error(error);
+        }
+        console.log(response);
+    });
+    disconnectFromBamazon();
 }
 
